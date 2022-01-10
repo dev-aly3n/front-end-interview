a461a64788633360841bcd2f51bf3c6d7fb5aebe
aly3n <aly3n.dev@gmail.com>
2022-01-10 21:51:23 +0330

    js-1

---
+++ JS/1/q.js
@@ -0,0 +1,21 @@
+// JavaScript provides a typeof operator that can examine a value and tell you what type it is:
+var a;
+typeof a;				// "undefined"
+
+a = "hello world";
+typeof a;				// "string"
+
+a = 42;
+typeof a;				// "number"
+
+a = true;
+typeof a;				// "boolean"
+
+a = null;
+typeof a;				// "object" -- weird, bug
+
+a = undefined;
+typeof a;				// "undefined"
+
+a = { b: "c" };
+typeof a;				// "object"
