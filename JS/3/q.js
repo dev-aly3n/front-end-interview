36027b9447df8c59c70db228b7c3b35217e514b8
aly3n <aly3n.dev@gmail.com>
2022-01-10 22:19:18 +0330

    js-3

---
+++ JS/3/q.js
@@ -0,0 +1,13 @@
+// An array is an object that holds values (of any type) not particularly in named properties/keys, but rather in numerically indexed positions:
+var arr = [
+	"hello world",
+	42,
+	true
+];
+
+arr[0];			// "hello world"
+arr[1];			// 42
+arr[2];			// true
+arr.length;		// 3
+
+typeof arr;		// "object"
